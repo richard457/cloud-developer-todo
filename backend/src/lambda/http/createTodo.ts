8455065896629e8faa70d71714cb75fde93cd3ca
Richard Muragijimana <beastar457@gmail.com>
2022-09-26 14:33:43 +0200

    fix the error in fake way first

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,8 +3,8 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+// import { getUserId } from '../utils';
+// import { createTodo } from '../../businessLogic/todos'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -12,6 +12,7 @@ export const handler = middy(
     // TODO: Implement creating a new TODO item
 
     return undefined
+  }
 )
 
 handler.use(
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,19 +4,25 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-import { getUserId } from '../utils';
+// import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+// import { getUserId } from '../utils';
 
 // TODO: Get all TODO items for a current user
+
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
+    const todoId = event.pathParameters.todoId
+    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+
 
     return undefined
-
-handler.use(
-  cors({
-    credentials: true
-  })
+  }
 )
+
+handler
+  // .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
